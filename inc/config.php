7c87af29e5a1b144ff743387549cebfb829c9c17
OLDI <oldi.prillo@gmail.com>
2019-11-15 15:26:15 +0000

    Added login and register php

---
+++ inc/config.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!defined('__CONFIG__')) {
+  exit("You are not the defined!!");
+}
+//CONFIG is complete
+
+ ?>
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+define("__CONFIG__",true);
+require_once "inc/config.php";
+
+ ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -15,37 +22,32 @@
   <body>
 
   	<div class="uk-section uk-container">
-  		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
-			<form class="uk-form-stacked js-login">
+  		<?php
+      echo "Hello World The Date is: ";
+      echo date('d m Y');
 
-			    <div class="uk-margin">
-			        <label class="uk-form-label" for="form-stacked-text">Email</label>
-			        <div class="uk-form-controls">
-			            <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
-			        </div>
-			    </div>
 
-			    <div class="uk-margin">
-			        <label class="uk-form-label" for="form-stacked-text">Password</label>
-			        <div class="uk-form-controls">
-			            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
-			        </div>
-			    </div>
+       ?>
 
-			    <div class="uk-margin">
-			        <button class="uk-button uk-button-default" type="submit">Login</button>
-			    </div>
+       <p>
 
-			</form>
-  		</div>
+
+         <a href="php_login_course/login.php">Login</a>
+         <a href="php_login_course/register.php">Register</a>
+
+
+
+
+       </p>
   	</div>
 
-    <!-- jQuery is required -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-    <!-- UIkit JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit-icons.min.js"></script>
+    <?php
+
+     require_once "inc/footer.php";
+
+     ?>
+
 
   </body>
 </html>
